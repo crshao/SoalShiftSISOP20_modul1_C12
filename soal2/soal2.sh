c6b6f37ab823c65a47000b46b766c31c4966d582
crshao <calvindon08@gmail.com>
2020-02-29 11:37:40 +0700

    perbaiki soal2

--- soal2/soal2.sh
+++ soal2/soal2.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-x=$(echo $1 | tr -d [:digit:])
-< /dev/urandom tr -dc A-Za-z0-9 | head -c28 > $x.txt
-y=`date +%H`
-echo "$x;$y" >> log.csv 
+a=$(echo $1 | tr -d [:digit:])
+p = "`< /dev/urandom tr -dc A-Z | head -c5`"
+q = "`< /dev/urandom tr -dc a-z | head -c5`"
+r = "`< /dev/urandom tr -dc 0-9 | head -c5`"
+s = "`< /dev/urandom tr -dc A-Za-z0-9 | head -c13`"
+pw = $p$q$r$s
+echo -e $pw > $a.txt
+b=`date +%H`
+echo "$a;$b" >> log.csv
